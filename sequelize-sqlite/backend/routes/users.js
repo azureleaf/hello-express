115f2613f455db747a7cdcaf797bb12215766be7
akb <doge.wow.so.scare@gmail.com>
2020-03-11 01:26:05 +0900

    feat: Register new user to the DB via Express & Axios

--- sequelize-sqlite/backend/routes/users.js
+++ sequelize-sqlite/backend/routes/users.js
@@ -2,13 +2,16 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models/"); // model/index.js内部の処理も使うので、必要なモデルだけでなくModelsディレクトリ全体を持ってくる
 
-/* GET users listing. */
+// 現在のDB内容を返す
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  models.user.findAll().then(users => {
+    res.json(users);
+  });
 });
 
-router.get("/add", function(req, res, next) {
-  console.log("userRouter!");
+// アクセスされるたびに固定メンバーを追加
+// テスト用。本番でこのようにgetに対してDBを操作したり、getに対してJSONを返したりはしない
+router.get("/addsato", function(req, res, next) {
   models.user
     .create({
       name: "佐藤一郎",
@@ -18,9 +21,26 @@ router.get("/add", function(req, res, next) {
       updated_at: new Date()
     })
     .then(() => {
-      return models.user.findAll();
+      res.redirect("/users/");
     })
-    .then(users => res.json(users))
+    .catch(err => {
+      console.error("エラーでした:", err);
+    });
+});
+
+// 実際に新しいメンバーを追加
+router.post("/add", function(req, res, next) {
+  models.user
+    .create({
+      name: req.body.name,
+      office: req.body.office,
+      gender: req.body.gender,
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+    .then(() => {
+      res.redirect("/users/"); // 更新後の名簿を返す
+    })
     .catch(err => {
       console.error("エラーでした:", err);
     });
