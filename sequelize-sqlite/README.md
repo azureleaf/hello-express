44bd4d8598acf965408098b23da26b6013467bc8
akb <doge.wow.so.scare@gmail.com>
2020-03-10 22:37:32 +0900

    doc: Add comments & notes

--- sequelize-sqlite/README.md
+++ sequelize-sqlite/README.md
@@ -5,9 +5,27 @@
 1. `npm install`
 
 
+## Notes
+
+- TypeORM CLIはExpressやDB連携の設定までやってくれるが、Sequelize CLIがやってくれるのはディレクトリ構造とconfigなどの雛形だけ
+    - Expressは別途自分で用意する必要がある
+-
+
 ## Installation History
 
+1. `npx express-generator --view=pug backend`
+1. `vue create frontend`
+
+### Backend
+1. `cd backend`
 1. `npm init`
 1. `npm i -S sequelize`
 1. `npm i -D sequelize-cli`
 1. `npx sequelize init`
+1. `npm install`
+1. `npm install sqlite3`
+1. `node ./bin/www`: Try to run
+1.　Edit `config/`
+1. `npx sequelize model:create --name user --underscored --attributes name:string,gender:string,office:string`
+    - Create model & its migration
+1. `npx sequelize db:migrate --env development`
--- sequelize-sqlite/backend/models/index.js
+++ sequelize-sqlite/backend/models/index.js
@@ -5,9 +5,13 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const config = require(__dirname + '/../config/config.json')[env]; // configファイルのdevelopment, test, productionの各内容がここに入る
+const db = {}; // このオブジェクトを最後にexportする
 
+
+// configファイルのhost, storageなどの部分にuse_env_variableという項目があるかどうかで場合分け
+// どうやらuse_env_variableに入るのはpostgres://username:password@localhost:5432/database_nameのような文字列らしい
+// つまり、else文にあるconfig.database, config.username, config.passwordの情報に相当
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,23 +19,29 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// fsはNode.jsのファイル読み書きモジュール
 fs
-  .readdirSync(__dirname)
+  .readdirSync(__dirname) // このindex.jsの場所が__dirname
   .filter(file => {
+    // Model定義ファイルの抽出
+    // blahblah.jsの形になっていないファイルを除外する
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
+    // モデルの一つ一つをfileとして取り出す
     const model = sequelize['import'](path.join(__dirname, file));
+    // dbは{"User": Userモデル定義, "Office": Officeモデル定義}のような構造になる
     db[model.name] = model;
   });
 
+// 各モデル内部にあるassociateメソッドを起動し、
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize;　// 
+db.Sequelize = Sequelize; // Sequelizeモジュール自体をdbのメンバに追加？？？なにそれ
 
 module.exports = db;
