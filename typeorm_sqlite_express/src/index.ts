96b5eba7c696525cc2382a5043da491e377d6f4e
akb <doge.wow.so.scare@gmail.com>
2020-03-13 23:56:05 +0900

    feat: Add seeder for user & office table

--- typeorm_sqlite_express/src/index.ts
+++ typeorm_sqlite_express/src/index.ts
@@ -4,8 +4,6 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
-import { Office } from "./entity/Office";
 import * as path from "path";
 import { resolveSoa } from "dns";
 
@@ -69,63 +67,8 @@ createConnection()
     // start express server
     app.listen(3000);
 
-    seeder.insertSeeds();
-
-    // insert new users for test
-    const user1 = new User();
-    user1.firstName = "義和";
-    user1.lastName = "鈴木";
-    user1.achievement = 80;
-    await connection.manager.save(user1);
-
-    const user2 = new User();
-    user2.firstName = "佐和子";
-    user2.lastName = "田中";
-    user2.achievement = 20;
-    await connection.manager.save(user2);
-
-    const user3 = new User();
-    user3.firstName = "大介";
-    user3.lastName = "佐藤";
-    user3.achievement = 0;
-    await connection.manager.save(user3);
-
-    const user4 = new User();
-    user4.firstName = "凛";
-    user4.lastName = "橋本";
-    user4.achievement = 100;
-    await connection.manager.save(user4);
-
-    // await connection.manager.save(
-    //   connection.manager.create(User, {
-    //     firstName: "義和",
-    //     lastName: "鈴木",
-    //     achievement: 80,
-    //   })
-    // );
-
-    // insert new offices for test
-
-    const office1 = new Office();
-    office1.name = "Sendai";
-    office1.users = [user1, user2];
-    await connection.manager.save(office1);
-
-    const office2 = new Office();
-    office2.name = "Nagamachi";
-    office2.users = [user3, user4];
-    await connection.manager.save(office2);
-
-    // await connection.manager.save(
-    //   connection.manager.create(Office, {
-    //     name: "Nagamachi"
-    //   })
-    // );
-    // await connection.manager.save(
-    //   connection.manager.create(Office, {
-    //     name: "Sendai"
-    //   })
-    // );
+    // Run seeder
+    seeder.run();
 
     console.log("Express server has started on port 3000");
   })
--- typeorm_sqlite_express/src/seeds.ts
+++ typeorm_sqlite_express/src/seeds.ts
@@ -1,12 +1,76 @@
-import { User } from "./entity/User";
 import { getConnection } from "typeorm";
+import { Office } from "./entity/Office";
+import { User } from "./entity/User";
 
-exports.insertSeeds = async () => {
-  // Insert the sample data
-  await getConnection()
+// インターフェースで二度手間で型を宣言しなくても、entityを使いまわして型定義できる気がするが...
+
+interface userObj {
+  firstName: string;
+  lastName: string;
+  achievement: number;
+}
+
+interface officeObj {
+  name: string;
+  users: userObj[];
+}
+
+const offices: officeObj[] = [
+  {
+    name: "仙台",
+    users: [
+      { firstName: "大介", lastName: "佐藤", achievement: 20 },
+      { firstName: "可奈子", lastName: "田中", achievement: 10 }
+    ]
+  },
+  {
+    name: "長町",
+    users: [
+      { firstName: "駿", lastName: "榊原", achievement: 99 },
+      { firstName: "安奈", lastName: "坂上", achievement: 10 }
+    ]
+  }
+];
+
+exports.run = async () => {
+
+  const connection = getConnection();
+
+  // 既存のデータを全て削除
+  await connection
     .createQueryBuilder()
-    .insert()
-    .into(User)
-    .values([{ firstName: "太郎", lastName: "伊集院", achievement: 90 }])
+    .delete()
+    .from(Office)
     .execute();
+  await connection
+    .createQueryBuilder()
+    .delete()
+    .from(User)
+    .execute();
+
+  // 上記のユーザーデータを元にOfficeとUserの両テーブルに挿入
+  offices.map(async office => {
+    // この事業所のentity
+    const officeObj = new Office();
+
+    // この事業所のUserのEntityを追加していく配列
+    const userObjs = [];
+    office.users.map(async (user, index) => {
+      userObjs.push(new User());
+
+      // それぞれのユーザーの情報を登録
+      userObjs[index].firstName = user.firstName;
+      userObjs[index].lastName = user.lastName;
+      userObjs[index].achievement = user.achievement;
+
+      await connection.manager.save(userObjs[index]);
+    });
+
+    // 事業所情報の登録
+    officeObj.name = office.name;
+    officeObj.users = userObjs;
+    await connection.manager.save(officeObj);
+  });
+
+  console.log("Seeding completed!");
 };
